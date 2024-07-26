c59d6f664e712aa69916d4a37bf79305b185e450
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-07-26 23:43:17 +0300

    Animation de la section tourisme

--- src/components/tourisme.tsx
+++ src/components/tourisme.tsx
@@ -1,33 +1,55 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import '../styles/components/_tourisme.scss'
 import tsingy from '../assets/images/Bemaraha/tsingy.jpg';
 import baobab from '../assets/images/Makay et allée des Baobabs/baobab.jpg';
 import vato from '../assets/images/Bemaraha/vato.jpg';
+import { AnimatePresence, motion } from 'framer-motion'
+import { variants_rond, variants_rond_creux, variants_slide, variantsImage_1, variantsImage_2, variantsImage_3 } from "../styles/animations/tourisme-variants";
 
 const Tourisme: FunctionComponent = () => {
+    const slideTourisme = [
+        {
+            title: 'TOURISME',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, esse blanditiis distinctio repellat, voluptas odio laboriosam magnam in sapiente consequuntur reiciendis architecto porro est, nam incidunt culpa. Voluptatibus, omnis voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit.Culpa perferendis totam vel suscipit voluptatibus possimus facilis! Eaque nihil nisi sapiente debitis!'
+        },
+        {
+            title: 'GEO-TOURISME',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, esse blanditiis distinctio repellat, voluptas odio laboriosam magnam in sapiente consequuntur reiciendis architecto porro est, nam incidunt culpa. Voluptatibus, omnis voluptates. Lorem ipsum dolor sit amet consectetur adipisicing elit.Culpa perferendis totam vel suscipit voluptatibus possimus facilis! Eaque nihil nisi sapiente debitis, quae est possimus! Tempora ipsa earum commodi repellendus perspiciatis!'
+        },
+    ]
+    // const [stateSlide, setStateSlide]  = useState<number>(0)
 
     return (
         <section className="flex flex-row justify-around px-5 pt-14 pb-20">
-            <div className="image ">
+            <motion.div initial='offscreen' whileInView='onscreen' viewport={{ amount: .8, once: true }} className="image">
                 <div className="flex flex-row">
-                    <div className="w-24 h-24 bg-secondary rounded-full"></div>
+                    <motion.div variants={variants_rond} className="w-24 h-24 bg-secondary rounded-full"></motion.div>
                     <div className="h-full flex justify-center pl-2">
-                        <img src={vato} alt="" className="w-96 h-96 rounded-full" />
+                        <motion.img variants={variantsImage_1} src={vato} alt="" className="w-96 h-96 rounded-full" />
                     </div>
                 </div>
                 <div className="relative">
-                    <img src={baobab} alt="" className="w-52 h-52 rounded-full absolute left-0 bottom-0 border-8 border-background "/>
-                    <img src={tsingy} alt="" className="tsingy w-32 h-32 rounded-full absolute border-4 border-background "/>
-                    <div className="pneu w-12 h-12"></div>
+                    <motion.img variants={variantsImage_2} src={baobab} alt="" className="w-52 h-52 rounded-full absolute left-0 bottom-0 border-8 border-background " />
+                    <motion.img variants={variantsImage_3} src={tsingy} alt="" className="tsingy w-32 h-32 rounded-full absolute border-4 border-background " />
+                    <motion.div variants={variants_rond_creux} className="pneu w-12 h-12"></motion.div>
                 </div>
-            </div>
-            <div className="slide px-2 flex flex-col justify-center">
-                <h1 className="text-3xl text-primary">TOURISME</h1>
-                <p className="text-black">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique, esse blanditiis distinctio repellat, voluptas odio laboriosam magnam in sapiente consequuntur reiciendis architecto porro est, nam incidunt culpa. Voluptatibus, omnis voluptates.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa perferendis totam vel suscipit voluptatibus possimus facilis! Eaque nihil nisi sapiente debitis, quae est possimus! Tempora ipsa earum commodi repellendus perspiciatis!
-                </p>
-            </div>
+            </motion.div>
+            <motion.div
+                initial="offscreen"
+                whileInView='onscreen'
+                viewport={{ amount: 0.5, once: true }}
+                variants={variants_slide}
+                className="slide flex flex-col justify-center"
+            >
+                <div className="mx-4">
+                    <h1 className="text-3xl text-primary">TOURISME</h1>
+                    <p className="text-black">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magni, unde cum! Dicta, voluptate! Magni doloribus perferendis autem officiis velit. Sint itaque minima nobis autem perspiciatis obcaecati dolore consequatur repellat architecto!
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus esse, ea, voluptas dignissimos fugit temporibus voluptate aliquid natus neque asperiores maxime. Velit sit a laudantium quisquam deleniti eligendi dicta illo!
+                    </p>
+                </div>
+
+            </motion.div>
         </section>
     )
 }
---
+++ src/styles/animations/tourisme-variants.tsx
@@ -0,0 +1,69 @@
+import { Variants } from "framer-motion"
+
+export const variants_slide: Variants = {
+    offscreen: { opacity: 0, y: 100 },
+    onscreen: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            bounce: .4,
+            duration: .8
+        }
+    }
+}
+export const variantsImage_1: Variants = {
+    offscreen: { rotate: 0, scale: 0 },
+    onscreen: {
+        rotate: 360, scale: 1,
+        transition: {
+            type: "spring",
+            bounce: .3,
+            duration: 1
+        }
+    }
+}
+export const variantsImage_2: Variants = {
+    offscreen: { x: -200, y: 100 },
+    onscreen: {
+        x: 0, y: 0,
+        transition: {
+            type: "tween",
+            duration: 0.8
+        }
+    }
+}
+
+export const variantsImage_3: Variants = {
+    offscreen: { x: 100, y: 100 },
+    onscreen: {
+        x: 0, y: 0,
+        transition: {
+            type: "tween",
+            duration: 0.6,
+            delay: .2
+        }
+    }
+}
+
+export const variants_rond: Variants = {
+    offscreen: { opacity: 0 },
+    onscreen: {
+        opacity: 1,
+        transition: {
+            duration: .3,
+            delay: .8
+        }
+    }
+}
+
+export const variants_rond_creux: Variants = {
+    offscreen: { opacity: 0 },
+    onscreen: {
+        opacity: 1,
+        transition: {
+            duration: .3,
+            delay: 1
+        }
+    }
+}
