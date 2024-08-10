0340e2d8bd2afa52f3793aa2231e7c3d94c255a0
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-08-10 17:57:55 +0300

    Ajout du profil dans navigation bar

--- src/components/navigation-bar.tsx
+++ src/components/navigation-bar.tsx
@@ -5,6 +5,7 @@ import { NavLink } from "react-router-dom";
 import { motion, useScroll, useSpring } from 'framer-motion'
 import { useLink } from "../hooks/useLink";
 import { useHeightNav } from "../hooks/useHeightNav";
+import { useAuth } from "../hooks/useAuth";
 
 const NavigationBar: FunctionComponent = () => {
     const refHeader = useRef<HTMLDivElement>(null)
@@ -29,6 +30,8 @@ const NavigationBar: FunctionComponent = () => {
     })
 
     const { links, linkActif, toggleLinkActif, toggleScrollActif } = useLink()
+
+    const { isAuth, clientAuth } = useAuth()
     return (
         <header className="flex flex-row justify-around" ref={refHeader}>
             <motion.div className="progress" style={{ scaleX: animateScaleX }}></motion.div>
@@ -44,10 +47,27 @@ const NavigationBar: FunctionComponent = () => {
                     )
                 }
             </nav>
-            <div className="flex items-center">
-                <NavLink to='/login' className='text-sm rounded-md px-4 py-2  transition ease-in duration-200 hover:bg-secondary hover:text-background mr-1'>Se connecter</NavLink>
-                <NavLink to='/signup' className="text-sm text-background rounded-md bg-primary px-6 py-2 transition ease-in duration-200 hover:bg-secondary">S'inscrire</NavLink>
-            </div>
+            {
+                !isAuth && (
+                    <div className="flex items-center">
+                        <NavLink to='/login' className='text-sm rounded-md px-4 py-2  transition ease-in duration-200 hover:bg-secondary hover:text-background mr-1'>Se connecter</NavLink>
+                        <NavLink to='/signup' className="text-sm text-background rounded-md bg-primary px-6 py-2 transition ease-in duration-200 hover:bg-secondary">S'inscrire</NavLink>
+                    </div>
+                )
+            }
+            {
+                isAuth && clientAuth && (
+                    <div className="flex items-center">
+                        <div className="py-1 px-4 flex flex-row items-center rounded-md bg-background">
+                            <p className="w-9 h-9 text-sm flex items-center justify-center bg-secondary border-2 border-primary rounded-full text-background capitalize">{clientAuth.name.split(' ').at(-1)?.substring(0, 2)}</p>
+                            <p className="w-20 mx-2 text-sm text-ellipsis overflow-hidden capitalize">{clientAuth.name.split(' ').at(-1)}</p>
+                            <i className="text-sm fa fa-caret-up mr-1"></i>
+                        </div>
+                    </div>
+
+                )
+            }
+
         </header >
 
 
