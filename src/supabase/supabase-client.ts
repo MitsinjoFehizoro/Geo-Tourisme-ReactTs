63feffec3400c9b828fcf9e83e1713f504831cec
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-08-22 21:29:25 +0300

    Configuration varaible d'environnement

--- src/supabase/supabase-client.ts
+++ src/supabase/supabase-client.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://kbjsuhybtvfubgbooxmp.supabase.co"
-const supabaseKeys = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtianN1aHlidHZmdWJnYm9veG1wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyNTg0MzcsImV4cCI6MjAzNzgzNDQzN30.IiShVWbURhuoxmDd646l9OVShbqtpRTtq7wdTc9BHOo"
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKeys = import.meta.env.VITE_SUPABASE_KEYS;
 
 export const supabase = createClient(supabaseUrl, supabaseKeys)
