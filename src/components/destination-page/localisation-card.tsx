12155a351fbe3f79cff5ef491c95f7b1a9795776
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-08-23 00:45:10 +0300

    Personnalisation de l'icon dans localisation-card

--- src/components/destination-page/localisation-card.tsx
+++ src/components/destination-page/localisation-card.tsx
@@ -1,18 +1,24 @@
 import { FunctionComponent } from "react";
 import 'leaflet/dist/leaflet.css'
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-
+import L from 'leaflet'
 type Props = {
     coordinates: number[],
     zoom: number
 }
 const LocalisationCard: FunctionComponent<Props> = ({ coordinates, zoom }) => {
+    const customIcon = new L.Icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, -32]
+    });
     return (
         <MapContainer center={[coordinates[0], coordinates[1]]} zoom={zoom} className="w-full h-full z-0" >
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <Marker position={[coordinates[0], coordinates[1]]}>
+            <Marker position={[coordinates[0], coordinates[1]]} icon={customIcon}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    {/* A pretty CSS3 popup. <br /> Easily customizable. */}
                 </Popup>
             </Marker>
         </MapContainer>
