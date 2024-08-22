9e8bac6f8ede8ae62e517266bbe3ee624ea150a3
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-08-23 01:14:58 +0300

    Debug de la responsivité de tourisme

--- src/components/tourisme/tourisme.tsx
+++ src/components/tourisme/tourisme.tsx
@@ -2,7 +2,7 @@ import { FunctionComponent } from "react";
 import '../../styles/components/_tourisme.scss';
 import { motion } from 'framer-motion';
 import { useLink } from "../../hooks/useLink";
-// import StyleImage from "./style-image";
+import StyleImage from "./style-image";
 import SlideTourisme from "./slide-tourisme";
 
 const Tourisme: FunctionComponent = () => {
@@ -14,7 +14,7 @@ const Tourisme: FunctionComponent = () => {
             onViewportEnter={() => toggleLinkActif('tourisme')}
             ref={links['tourisme'].refDestination}
             className="relative bg-background flex flex-col lg:flex-row items-center justify-around px-4 pt-14 pb-12 lg:pb-16">
-            {/* <StyleImage /> */}
+            <StyleImage />
             <SlideTourisme />
         </motion.section>
     );
--- src/styles/animations/tourisme-variants.ts
+++ src/styles/animations/tourisme-variants.ts
@@ -39,9 +39,9 @@ export const variantsImage_2: Variants = {
 }
 
 export const variantsImage_3: Variants = {
-    offscreen: { opacity: 0, x: 100, y: 100 },
+    offscreen: { opacity: 0, scale: 0, x: 100, y: 100 },
     onscreen: {
-        opacity: 1, x: 0, y: 0,
+        opacity: 1, x: 0, y: 0, scale: 1,
         transition: {
             type: "tween",
             duration: 0.6,
