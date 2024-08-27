9c44816897c9988e6c1d033cabbaa1630499d0b8
MitsinjoFehizoro <mitsinjofehizoro@gmail.com>
2024-08-27 08:27:32 +0300

    Correctiond du bug de l'animation

--- src/components/accueil/introduction.tsx
+++ src/components/accueil/introduction.tsx
@@ -11,7 +11,9 @@ const Introduction: FunctionComponent = () => {
             {
                 stateGetInformation.isLoading ? (
                     <>
-                        <h1 className="w-full h-5 mb-6 bg-background animate-pulse" />
+                        <h1 className="w-full h-5 mb-4 bg-background animate-pulse" />
+                        <h1 className="w-1/2 h-5 md:h-0 bg-background animate-pulse" />
+                        <p className="w-full h-6"/>
                         {Array.from({ length: 7 }).map((_, index) => (
                             <p key={index} className="w-full bg-background h-2 mb-4" />
                         ))}
